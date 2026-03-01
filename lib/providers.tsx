88ddb9b7a93f7e82e1c4025625f898b30f15da2a
Saber1Y <ayodejiakintobi1@gmail.com>
2026-03-01 09:16:25 +0100

    feat: somnia config

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -12,6 +12,22 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { http } from "viem";
 import { somnia } from "./config";
 
+const somniaEvmNetwork = {
+  blockExplorerUrls: ["https://shannon-explorer.somnia.network/"],
+  chainId: 50312,
+  chainName: "Somnia Testnet",
+  iconUrls: ["https://app.dynamic.xyz/assets/networks/somnia.svg"],
+  name: "Somnia",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Somnia Test Token",
+    symbol: "STT",
+  },
+  networkId: 50312,
+  rpcUrls: ["https://dream-rpc.somnia.network/"],
+  vanityName: "Somnia Testnet",
+};
+
 const config = createConfig({
   chains: [somnia] as const,
   multiInjectedProviderDiscovery: false,
@@ -28,6 +44,9 @@ export function Providers({ children }: { children: ReactNode }) {
       settings={{
         environmentId: process.env.NEXT_PUBLIC_DYNAMIC_ID || "",
         walletConnectors: [EthereumWalletConnectors],
+        overrides: {
+          evmNetworks: [somniaEvmNetwork],
+        },
       }}
     >
       <WagmiProvider config={config}>
